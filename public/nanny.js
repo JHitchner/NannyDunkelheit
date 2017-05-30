5787d1c6ca3c51c992a634d6072521f411e515ab
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-05-29 23:21:05 -0400

    final page addedfinal page added, contact page added, links added, animations, audio, nav

--- public/nanny.js
+++ public/nanny.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var test = document.getElementById("test");
   var scene = document.getElementById("scene_container");
-  var buttonStart = document.getElementById("butt");
+  var buttonStart = document.getElementById("door");
   var trialType = document.getElementById("scene1Type").textContent;
   var trialArray = trialType.split("");
   var show = document.getElementById("showType");
   var blockY = document.getElementById("nannyBlock");
-  var proceedBtn = document.getElementById("startScroll");
+  // var proceedBtn = document.getElementById("startScroll");
   var darknessAni =document.getElementById("darkness");
   var body =document.getElementsByTagName("body");
   var musicBox =document.getElementById("musicBox");
   var storm =document.getElementById("storm");
-  var nannyAni = document.getElementById("nanny");
+  var nannyAni = document.getElementById("nanny_sil");
+  var nannySprites =document.getElementById("nanny_sil3")
   var children =document.getElementById("children1");
   var creak =document.getElementById("creak");
   var knock =document.getElementById("knock");
@@ -24,7 +25,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var main =document.getElementById("main");
   var whisperOpen=document.getElementById("whisper_open");
 var openBG=document.getElementById("bg1");
-var deadHead =document.getElementById("bg6");
+var deadHead =document.getElementById("deadbust");
+var head =document.getElementById("bust");
 var nannyPiper =document.getElementById("nanny_piper");
 var stairs =document.getElementById("stairs");
 var nannyPiper2 =document.getElementById("nannyPiper2");
@@ -34,6 +36,10 @@ var lightsOut =document.getElementById("lightsOut");
 var transform =document.getElementById("transform");
 var bgMove =document.getElementById("bg7");
 var transFormWav =document.getElementById("transformWav");
+var bedtime =document.getElementById("bedtimeWav");
+var shadowAni =document.getElementById("feelers4");
+var nomNomAni =document.getElementById("nomNom");
+var nannArms =document.getElementById("nannyArms");
 // Function that prints the text of the story one character at a time.
   function pageOne(){
      var i = 0;
@@ -58,36 +64,54 @@ var transFormWav =document.getElementById("transformWav");
       main.play();
       console.log("bam");
     }
-    if (scene.scrollLeft < 4000 && scene.scrollLeft > 3900){
-      main.play();
-      console.log("bam");
+    if (scene.scrollLeft < 5600 && scene.scrollLeft > 5500){
+      children.style.animationPlayState = "running";
     }
     if (scene.scrollLeft < 6980 && scene.scrollLeft > 6880){
       nannyPiper.style.animationPlayState = "running";
       stairs.play();
+      main.pause();
+      window.setTimeout(function() {
+        shadowAni.style.animationPlayState = "running";
+      }, 7000);
     }
     if (scene.scrollLeft < 7800 && scene.scrollLeft > 7700){
       nannyPiper2.style.animationPlayState = "running";
-      main.pause();
       downHall.play();
-      creak.play();
+      stairs.pause();
       window.setTimeout(function() {
-        stairs.play();
-      }, 3000);
+        deadHead.style.display = "block";
+        head.style.display = "none";
+      }, 8500);
     }
   }
-  spriteBtn.addEventListener("click", function(){
-    nannyShadowSprite.style.animationPlayState = "running";
+  nannyPiper2.addEventListener("animationend", function(){
+    bedtimeWav.play();
   })
+  // spriteBtn.addEventListener("click", function(){
+  //   nannyShadowSprite.style.animationPlayState = "running";
+  // })
+  children.addEventListener("animationend", function(){
+    nannyShadowSprite.style.animationPlayState = "running";
+  });
+  var hudClick =document.getElementById("clickY");
+  var hud1 = window.setTimeout(function(){
+      hudClick.style.display = "block";
+    }, 8000);
 
   nannyAni.addEventListener("animationend", pageOne);
+  // var hudClick2 =document.getElementById("clickDoor");
+  // var woo = window.setTimeout(function(){
+  //     hudClick2.style.display = "block";
+  //   }, 12000);
 // Remove Block,Trigger HomePage Animtion and allow scroll
-  proceedBtn.addEventListener("click", function(){
-    proceedBtn.style.display = "none";
+  blockY.addEventListener("click", function(){
+    window.clearTimeout(hud1);
+    clickY.style.display ="none";
     darknessAni.style.animationPlayState = "running";
     window.setTimeout(function() {
       blockY.style.display = "none";
-      openBG.style.background = "url('images/homePageNanny2.jpg') center center no-repeat";
+      openBG.style.backgroundImage = "url('images/homePageNannyBlood.png')";
     }, 2000);
     scene.style.whiteSpace = "nowrap"
     creak.play();
@@ -96,6 +120,8 @@ var transFormWav =document.getElementById("transformWav");
   });
 
   buttonStart.addEventListener("click", function(){
+    // window.clearTimeout(woo);
+    // hudClick2.style.display = "none";
     knock.play();
     window.setTimeout(function() {
       mom.style.animationPlayState = "running"
@@ -112,6 +138,13 @@ lightBtn.addEventListener("click", function(){
   transform.style.animationPlayState ="running";
   bgMove.style.animationPlayState ="running";
   transFormWav.play();
+  bedtimeWav.pause();
+  nomNomAni.style.display = "block";
+  nomNomAni.style.animationPlayState = "running";
+  window.setTimeout(function() {
+    nannArms.style.display = "block";
+    nannArms.style.animationPlayState = "running";
+  }, 4000);
 });
 
 
